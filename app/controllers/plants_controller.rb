80ca194027c9468a2f14b86a77416d7c85af94e0
Aya Ramirez <ayaramz@gmail.com>
2022-06-29 23:04:07 -0400

    commit tree e80f145249d703ef8837082e5d5936e76afb630f

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,7 +8,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -18,9 +18,40 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update 
+    plant = find_plant
+    if plant 
+      plant.update(plant_params)
+      render json: plant
+    else
+      render_not_found_response
+    end
+  end
+
+  def destroy
+    plant = find_plant
+    if plant
+        plant.destroy
+        head :no_content
+    else
+      render_not_found_response
+    end
+  end
+
+
   private
+
+  def find_plant
+    Plant.find_by(id: params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def render_not_found_response
+    render json: {error: "OPE. Plant not here buddy."}, staus: :not_found
+  end 
+
 end
+
